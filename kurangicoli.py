1131a3370e953ea2357ca122aca68899faf21a80
alifnakal <77843128+alifnakal@users.noreply.github.com>
2021-02-04 09:47:08 +0700

    Add files via upload

---
+++ kurangicoli.py
@@ -0,0 +1,263 @@
+#!/system/xbin/bash
+#Bersihkan Layar
+clear
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+sleep 1
+echo "\033[32;1m=
+  __________________________
+  | $ Pulsa Gratis                |
+  | $ BY CHEATERDESA     |
+  |                                          |
+  |                                          |
+  |________________________|
+     _____||_______||____                 
+     |__________________|
+"
+toilet -f standard -F gay "CHEATERDESA022"
+echo "\033[31;1m=
+
+
+        SUBSCRIBE CHEATER DESA022
+
+
+"
+echo ""
+echo "By : " CHEATERDESA "
+" Team : "NGENTOD.ID"
+echo " Gmail : "arivinsantos4@gmail.com"
+" Pesan : "Jangan Di Salah Gunakan NTOD:)"
+sleep 1
+echo "ThanksTo:"
+echo "CHEATERDESA"
+echo "\033[32;1m=
+  __________________________
+  | $ Pulsa Gratis                |
+  | $ BY CHEATER DESA    |
+  |                                          |
+  |                                          |
+  |________________________|
+     _____||_______||____                 
+     |__________________|
+"
+echo ""
+sleep 3
+echo "HACK PULSA CHEATER DESA"
+echo " Pakai +62 NTOD#Contoh +62xxx:"
+read p;
+echo "SABAR Woi PROSES"
+sleep 2
+echo "\033[37;1mANDA  10%" 
+sleep 1
+echo "G  20%" 
+sleep 1
+echo "O  30%" 
+sleep 3
+echo "B  40%" 
+sleep 3
+echo "L  50%" 
+sleep 3
+echo "O  60%"
+echo "K  90%"
+sleep 5
+echo "\033[31;1mNomor BERHASIL Njeng...""\033[32;1mMASUK KAN LAGI NOMOR MU COK:"
+read p;
+sleep 3
+echo "Sabar lagi Proses..."
+sleep 3
+echo "\033[37;1m#10%"
+sleep 2
+echo "##20%"
+sleep 5
+echo "###30%"
+sleep 3
+echo "####40%"
+sleep 3
+echo "#####50%"
+sleep 3
+echo "######60%"
+sleep 3
+echo "#######70%"
+sleep 4
+echo "########80%"
+sleep 3
+echo "#########90%"
+sleep 4
+echo "##########100%"
+sleep 3
+echo "\033[32;1mBerapa Pulsa Yang Kau Mau :"
+read p;
+sleep 2
+echo "Sabar lagi  Proses..."
+echo "\033[37;1m#10%"
+sleep 1
+echo "##20%"
+sleep 1
+echo "###30%"
+sleep 3
+echo "####40%"
+sleep 3
+echo "#####50%"
+sleep 3
+echo "######60%"
+sleep 3
+echo "#######70%"
+sleep 4
+echo "########80%"
+sleep 3
+echo "#########90%"
+sleep 4
+echo "##########100%"
+sleep 6
+echo "\033[34;1mSukses Mamank...""\033[32;1m Tunggu 1- 24 Jam ..."
+sleep 2
+sleep 3
+echo "Ketik Exit Untuk Keluar"
+read p;
+sleep 2
+sleep 3
+sleep 4
+echo "Pesan:Jika Script Ini Tidak Berfungsi Pada Anda Berarti anda Goblok"
+sleep 3
+echo "JANGAN COLI TRS COK"
+sleep 2
+echo "thanks for NGENTOD.DI TEAM"
+sleep 2
+sleep 4
+exitPulsa-#!/system/xbin/bash
+#Bersihkan Layar
+clear
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+sleep 1
+echo "\033[32;1m=
+  __________________________
+  | $ Pulsa Gratis                  |
+  | $ BY CHEATER DESA      |
+  |                                            |
+  |                                            |
+  |________________________|
+     _____||_______||____                 
+     |__________________|
+"
+toilet -f standard -F gay "MR Thunder"
+echo "\033[31;1m=
+
+
+SUBSCRIBE CHEATER DESA022
+
+
+"
+echo ""
+echo "By : " CHEATER DESA"
+" Team : "NGENTOD.ID"
+echo " Gmail : "arivinsantos4@gmail.com"
+" Pesan : "Jangan Di Salah Gunakan TOD:)"
+sleep 1
+echo "ThanksTo:"
+echo "MR ASU"
+echo "\033[32;1m=
+  __________________________
+  | $ Pulsa Gratis                 |
+  | $ BY CH                           |
+  |                                          |
+  |                                          |
+  |________________________|
+     _____||_______||____                 
+     |__________________|
+"
+echo ""
+sleep 3
+echo "HACK PULSA CHEATER DESA022"
+echo " Pakai +62 Abang #Contoh +62xxx:"
+read p;
+echo "SABAR Woi PROSES"
+sleep 2
+echo "\033[37;1mANDA  10%" 
+sleep 1
+echo "G  20%" 
+sleep 1
+echo "O  30%" 
+sleep 3
+echo "B  40%" 
+sleep 3
+echo "L  50%" 
+sleep 3
+echo "O  60%"
+echo "K  90%"
+sleep 5
+echo "\033[31;1mNomor SUKSES Njeng...""\033[32;1mSilahkan Masukkan Kembali No MU:"
+read p;
+sleep 3
+echo "Sabar lagi Proses..."
+sleep 3
+echo "\033[37;1m#10%"
+sleep 2
+echo "##20%"
+sleep 5
+echo "###30%"
+sleep 3
+echo "####40%"
+sleep 3
+echo "#####50%"
+sleep 3
+echo "######60%"
+sleep 3
+echo "#######70%"
+sleep 4
+echo "########80%"
+sleep 3
+echo "#########90%"
+sleep 4
+echo "##########100%"
+sleep 3
+echo "\033[32;1mBerapa Pulsa Yang Kau Mau :"
+read p;
+sleep 2
+echo "Sabar lagi  Proses..."
+echo "\033[37;1m#10%"
+sleep 1
+echo "##20%"
+sleep 1
+echo "###30%"
+sleep 3
+echo "####40%"
+sleep 3
+echo "#####50%"
+sleep 3
+echo "######60%"
+sleep 3
+echo "#######70%"
+sleep 4
+echo "########80%"
+sleep 3
+echo "#########90%"
+sleep 4
+echo "##########100%"
+sleep 6
+echo "\033[34;1mSukses Mamank...""\033[32;1m Tunggu 1 -24 JAM..."
+sleep 2
+sleep 3
+echo "Ketik Exit Untuk Keluar"
+read p;
+sleep 2
+sleep 3
+sleep 4
+echo "Pesan:Jika Script Ini Tidak Berfungsi Pada Anda Berarti anda Goblok"
+sleep 3
+echo "Selamat KURANGI COLI"
+sleep 2
+echo "ThanksTo CH "
+sleep 2
+sleep 4
+exit
